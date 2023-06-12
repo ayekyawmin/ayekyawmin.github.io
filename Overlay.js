b2bcbe5c1ed7c7e89a4d4734284fcbab609cfa1f
AyeKyawMin <65951104+ayekyawmin@users.noreply.github.com>
2023-06-13 00:34:46 +0630

    Add files via upload

---
+++ Overlay.js
@@ -0,0 +1,37 @@
+export function Overlay() {
+  return (
+    <>
+
+      <div className="headd" style={{ top: 21, left: '5vw' }}>
+        <img src='summ.png' alt="My img" width="900" height="auto" ></img>
+      </div>
+      
+      <div className="char2" style={{ top: 160, left: '3vw' }}>
+      <i><p>Designed as per the consumers' needs with high-quality standards.</p></i>
+      <i><p>Products made by the team of skilled professionals.</p></i>
+      <i><p>Produced by high precision tools with quality control.</p></i>
+      <i><p>On-site installation for customer's satisfaction.</p></i><br /><br /><br /><br /><br />
+      </div>
+      
+      <div className="char3" style={{ top: 430, left: '3vw' }}>
+            <h3>Address</h3>
+      <p>No.225, Thanlwin Street, Ward(64),</p>
+      <p>Industrial Zone(2), South Dagon, </p>
+      <p>Yangon, Myanmar</p>
+      <br></br><br /><br />
+      <h3> Call Us </h3>
+      <p>+959 5056590,</p>
+      <p>+959 5069058,</p>
+      <p>+959 448046082</p>
+
+      </div>
+
+      <div className="eml" style={{ bottom: 125, left: '3vw' }}>
+        <i>summationmyanmar@gmail.com</i>
+      </div>
+
+
+
+    </>
+  )
+}
